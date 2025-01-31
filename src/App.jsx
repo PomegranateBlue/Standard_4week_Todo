6fbb6f884e286a415298547d17becfaefd48500f
bluepome98 <wpdn1998@gmail.com>
2025-01-31 20:55:56 +0900

    다시 구조짜기

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,6 @@ function App() {
   console.log(todo);
   //할 일 목록의 useState, 추가하는 useState는 별도 생성
 
-  const [newTodo, setNewTodo] = useState({
-    id: Date.now(),
-    title: "",
-    content: "",
-  });
-
-  const addTodo = (e) => {
-    e.preventDefault();
     setTodo((item) => [...item, newTodo]);
   };
   //추가 시 사용하는 상태
@@ -32,13 +24,17 @@ function App() {
         <input
           type="text"
           value={newTodo.title}
-          onChange={(e) => setNewTodo({ ...todo, title: e.target.value })}
+          onChange={(e) =>
+            setNewTodo((item) => ({ ...item, title: e.target.value }))
+          }
         ></input>
         <label>내용</label>
         <input
           type="text"
           value={newTodo.content}
-          onChange={(e) => setNewTodo({ ...todo, content: e.target.value })}
+          onChange={(e) =>
+            setNewTodo((item) => ({ ...item, content: e.target.value }))
+          }
         ></input>
         <button onClick={addTodo}>추가</button>
         <div className="working-carousel">
