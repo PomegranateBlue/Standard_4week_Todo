bd61eb72aff0d600ba03ca3278a02c4fb96fb41f
bluepome98 <wpdn1998@gmail.com>
2025-02-01 00:04:58 +0900

    2025-02-01

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
 import "./App.css";
+import { useState } from "react";
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "test Title",
+      content: "test Content",
+    },
+  ]);
+
+  console.log(todos);
   return (
     <div>
       <h1>TODO LIST</h1>
       <div className="form-container">
         <form>
           <label>제목</label>
-          <input></input>
+          <input type="text"></input>
           <label>내용</label>
-          <input></input>
+          <input type="text"></input>
           <button>추가</button>
         </form>
       </div>
       <div className="todoList-container">
-        <div className="working-container"></div>
-        <div className="done-container"></div>
+        <div className="working-container">
+          <h2>Working...</h2>
+          {todos.map((item) => (
+            <li key={item.id}>
+              title:{item.title} - content:{item.content}
+            </li>
+          ))}
+        </div>
+        <div className="done-container">
+          <h2>Done!</h2>
+        </div>
       </div>
     </div>
   );
