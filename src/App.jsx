91cca27cb16930f2fdedb6a38b79b9afdc092004
bluepome98 <wpdn1998@gmail.com>
2025-01-31 20:16:53 +0900

    현재 input 태그에 [object Object] 표시, value값 문제

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,38 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      title: "test title",
+      content: "test content",
+    },
+  ]);
+
   return (
     <div>
       <h1>TODO LIST</h1>
       <div className="inputForm">
         <label>제목</label>
-        <input></input>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        ></input>
         <label>내용</label>
-        <input></input>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        ></input>
         <button>추가</button>
-        <div className="working-carousel"></div>
+        <div className="working-carousel">
+          {todo.map((item) => (
+            <li key={item.id}>
+              title:{item.title} content:{item.content}
+            </li>
+          ))}
+        </div>
         <div className="done-carousel"></div>
       </div>
     </div>
