4810e082caed30e9677e2964fc4e3bc1fe22c4a5
bluepome98 <wpdn1998@gmail.com>
2025-02-02 14:13:41 +0900

    삭제 로직 확인인

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,49 @@ const App = () => {
     },
   ]);
 
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+      id: Date.now(),
+      title: title,
+      content: content,
+    };
+    setTodos([...todos, newTodo]);
+    resetForm();
+  };
   console.log(todos);
+
+  const deleteTodo = (id) => {
+    //삭제 버튼을 누른 항목의 id 조회
+    //해당 id를 제외한 todos 목록 업데이트
+    setTodos(todos.filter(({ item }) => item.id !== id));
+  };
+  const resetForm = () => {
+    setTitle("");
+    setContent("");
+  };
   return (
     <div>
       <h1>TODO LIST</h1>
       <div className="form-container">
         <form>
           <label>제목</label>
-          <input type="text"></input>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
           <label>내용</label>
-          <input type="text"></input>
-          <button>추가</button>
+          <input
+            type="text"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          ></input>
+          <button onClick={addTodo}>추가</button>
         </form>
       </div>
       <div className="todoList-container">
@@ -28,6 +60,7 @@ const App = () => {
           {todos.map((item) => (
             <li key={item.id}>
               title:{item.title} - content:{item.content}
+              <button onClick={deleteTodo}>제거</button>
             </li>
           ))}
         </div>
